d58b61c3cf0844a2fda11d390f70ea6bcc4d2e84
Priyanshu Batham <priyanshubatham24733@gmail.com>
2025-02-03 09:02:57 +0530

    Dockerized the Project (#86)
    * Checkpoint one
    
    * fixed the docker-compose stuff.
    it was being a development container so i changed it to production containers
    
    * dockerised the whole project
    
    ---------
    
    Co-authored-by: vaibhavs-mishra <vaibhavs.mishra@proton.me>

---
+++ Algolyzer/Algolyzer/settings/production.py
@@ -0,0 +1,58 @@
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+CSRF_TRUSTED_ORIGINS = os.getenv(
+    "CSRF_TRUSTED_ORIGINS",
+    "http://localhost,http://127.0.0.1,http://localhost:8000,http://127.0.0.1:8000",
+).split(",")
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+# CSRF_COOKIE_SECURE = False  # Since you're using HTTP (for now)
+# SESSION_COOKIE_SECURE = False  # Since you're using HTTP (for now)
+
+# To be done like this in future:
+# from .base import *
+
+# DEBUG = False
+
+# ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+
+# for https:
+# SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": os.getenv("DB_NAME"),
+#         "USER": os.getenv("DB_USER"),
+#         "PASSWORD": os.getenv("DB_PASSWORD"),
+#         "HOST": os.getenv("DB_HOST"),
+#         "PORT": os.getenv("DB_PORT", "5432"),
+#     }
+# }
+
+# SECURE_BROWSER_XSS_FILTER = True
+# SECURE_CONTENT_TYPE_NOSNIFF = True
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
+# X_FRAME_OPTIONS = "DENY"
+
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# EMAIL_HOST = os.getenv("EMAIL_HOST")
+# EMAIL_PORT = os.getenv("EMAIL_PORT")
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+# EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
---
+++ Dockerfile
@@ -0,0 +1,63 @@
+# Use official Python image with Alpine as base
+FROM python:3.12-alpine
+
+# Set environment variables
+ENV PYTHONUNBUFFERED=1 
+
+# Set the working directory
+WORKDIR /app
+
+# Install system dependencies
+RUN apk update && \
+    apk add --no-cache \
+    curl \
+    nginx \
+    bash \
+    nodejs \
+    npm && \
+    rm -rf /var/cache/apk/*
+
+# Install specific npm version
+RUN npm install -g npm@10.9.2 
+
+# Copy requirements.txt
+COPY ./requirements.txt /app/
+
+# Install Python dependencies
+RUN pip install --no-cache-dir -r requirements.txt gunicorn
+
+# Install Node.js dependencies (Tailwind, etc.)
+COPY ./package.json /app/
+RUN npm install
+
+# Copy project files
+COPY . /app/
+
+# Run tailwind build
+RUN npm run tw_build
+
+# Change working directory
+WORKDIR /app/Algolyzer/
+
+# Migrate database
+RUN python manage.py migrate
+
+# Set super user details
+ENV DJANGO_SUPERUSER_USERNAME=admin \
+    DJANGO_SUPERUSER_EMAIL=admin@gmail.com \
+    DJANGO_SUPERUSER_PASSWORD=admin
+
+# Seed the database
+RUN python manage.py loaddata data/*
+
+# Collect static files
+RUN python manage.py collectstatic --noinput
+
+# Expose port 8000
+EXPOSE 8000
+
+# Use production settings instead of default development settings       
+ENV DJANGO_SETTINGS_MODULE=Algolyzer.settings.production
+
+# Start the application
+CMD ["gunicorn", "--bind", "0.0.0.0:8000", "Algolyzer.wsgi:application"]
