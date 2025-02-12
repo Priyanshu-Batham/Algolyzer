b8c3fad44bbc15be3b3086862fda0c5ca9edc8da
Priyanshu-Batham <priyanshubatham24733@gmail.com>
2025-02-12 15:52:27 +0530

    added the first working model along with designed templates

--- Algolyzer/playground/views.py
+++ Algolyzer/playground/views.py
@@ -1,5 +1,7 @@
+import json
+
 from django.shortcuts import redirect, render
-from playground.tasks import process_ml_task
+from playground.tasks import sentiment_analysis_task
 
 from .models import PlaygroundTask
 
@@ -11,50 +13,46 @@ def playground_home(request):
 
 
 # Task Submission View
-def submit_task_view(request):
-    """Handles ML model submission through a form."""
-    if request.method == "POST":
-        user = request.user  # Ensure the user is authenticated
-        input_data = request.POST.get("input_data")
-        model_name = request.POST.get("model_name", "model_1")  # Default to model_1
 
-        if model_name not in ["model_1", "model_2"]:  # Validate model selection
-            return render(
-                request, "playground/home.html", {"error": "Invalid model selected"}
-            )
+
+def sentiment_analysis(request):
+    """Handles sentiment analysis model submission and task status retrieval."""
+    user = request.user  # Ensure the user is authenticated
+
+    if request.method == "POST":
+        input_data = request.POST.get("input_data")
 
         # Create a task entry in the database
         task = PlaygroundTask.objects.create(
             user=user,
-            model_name=model_name,
             input_data=input_data,
+            model_name="sentiment_analysis",
             status="PENDING",
         )
 
         # Call Celery and attach the Celery task ID to the database entry
-        celery_task = process_ml_task.apply_async(
-            args=[task.id, model_name, input_data]
-        )
+        celery_task = sentiment_analysis_task.apply_async(args=[task.id, input_data])
         task.celery_task_id = celery_task.id
         task.save()
 
-        return redirect("task_status_view", task_id=task.celery_task_id)
+        return redirect("sentiment_analysis")
 
-    return render(request, "playground/home.html")
+    else:
+        """Handles task status retrieval."""
 
+        previous_results = PlaygroundTask.objects.filter(
+            user=user, model_name="sentiment_analysis"
+        ).order_by("-created_at")
 
-# Task Status View
-def check_task_status_view(request, task_id):
-    """Renders the task status page."""
-    try:
-        task = PlaygroundTask.objects.get(celery_task_id=task_id)
-        context = {
-            "task_id": task.celery_task_id,
-            "status": task.status,
-            "result": task.result if task.status == "COMPLETED" else None,
-        }
-        return render(request, "playground/task_status.html", context)
-    except PlaygroundTask.DoesNotExist:
-        return render(
-            request, "playground/task_status.html", {"error": "Task not found"}
-        )
+        for task in previous_results:
+            if task.result:
+                try:
+                    # Replace single quotes with double quotes to make it valid JSON
+                    formatted_result = task.result.replace("'", '"')
+                    task.result = json.loads(formatted_result)
+                except json.JSONDecodeError:
+                    task.result = (
+                        {}
+                    )  # If JSON is invalid, set it as an empty dictionary
+        context = {"previous_results": previous_results}
+        return render(request, "playground/sentiment_analysis.html", context=context)
