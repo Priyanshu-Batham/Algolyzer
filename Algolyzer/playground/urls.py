553e6c0f3f217c98892f4c7693b68dcdd1a532f6
Priyanshu Batham <priyanshubatham24733@gmail.com>
2025-04-16 12:28:33 +0530

    added new model kmeans clustering (#142)

--- Algolyzer/playground/urls.py
+++ Algolyzer/playground/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from .views import (
     doodle_classifier,
+    kmeans_clustering,
     linear_regression,
     playground_home,
     sentiment_analysis,
@@ -10,6 +11,7 @@ from .views import (
 urlpatterns = [
     path("", playground_home, name="playground_home"),
     path("linear_regression", linear_regression, name="linear_regression"),
+    path("kmeans_clustering", kmeans_clustering, name="kmeans_clustering"),
     path("sentiment_analysis/", sentiment_analysis, name="sentiment_analysis"),
     path("doodle_classifier/", doodle_classifier, name="doodle_classifier"),
 ]
