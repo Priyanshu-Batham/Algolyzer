0e838fb841fd7d439e19ae7e7c96b3e8bc5f6409
Vaibhav Shankar Mishra <vaibhavs.mishra@proton.me>
2025-04-21 00:55:36 +0530

    Added new material: 1. Doodle classifier 2. Linear Regression 3. K-means Clustering 4. SVM Regression
    Updated existing material with better explanation:
    1. Sentiment Analysis

--- Algolyzer/study/views.py
+++ Algolyzer/study/views.py
@@ -7,3 +7,19 @@ def home(request):
 
 def sentiment_analysis(request):
     return render(request, "study/sentiment_analysis.html")
+
+
+def doodle_classifier(request):
+    return render(request, "study/doodle_classifier.html")
+
+
+def linear_regression(request):
+    return render(request, "study/linear_regression.html")
+
+
+def k_means_clustering(request):
+    return render(request, "study/k_means_clustering.html")
+
+
+def svm_regression(request):
+    return render(request, "study/svm_regression.html")
