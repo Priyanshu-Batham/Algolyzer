b9dad35ff00a76a7ea1c2bf9cb3113cb08c65856
vaibhavs-mishra <vaibhavs.mishra@proton.me>
2025-04-21 20:25:22 +0530

    Added new material: (#144)
    1. Doodle classifier
    2. Linear Regression
    3. K-means Clustering
    4. SVM Regression
    
    Updated existing material with better explanation:
    1. Sentiment Analysis

--- Algolyzer/study/urls.py
+++ Algolyzer/study/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path(
         "sentiment_analysis/", views.sentiment_analysis, name="sentiment_analysis_study"
     ),
+    path("doodle_classifer/", views.doodle_classifier, name="doodle_classifier_study"),
+    path("linear_regression/", views.linear_regression, name="linear_regression_study"),
+    path("k_means_clustering/", views.k_means_clustering, name="k_means_clustering"),
+    path("svm_regression/", views.svm_regression, name="svm_regression"),
 ]
